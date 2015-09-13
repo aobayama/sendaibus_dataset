069e017f2accf9dba0e314a35d35f05603cd885e
Rintaro Okamura <rintaro.okamura@gmail.com>
2015-09-13 13:12:00 -0600

    Add apply_kana_to_json.rb - it generates merged kana-data: coords_yomigana.json

---
+++ bin/apply_kana_to_json.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'json'
+require 'csv'
+
+source_csv = "../csv/yomigana.csv"
+# "busstop_name", "yomigana"
+source_json = "../json/coords.json"
+# { "unique_id" : { "coordinates" : {"lat": lat, "lon": lon}, "name": name}}
+target_json = "../json/coords_yomigana.json"
+# { "unique_id" : { "coordinates" : {"lat": lat, "lon": lon}, "name": name, "yomi": yomi}}
+
+# ---------
+
+dump_data   = Hash.new()
+source_data = Hash.new()
+target_data = Hash.new() # => merged data
+
+CSV.foreach(source_csv, "r") do |row|
+  dump_data.store(row[0],row[1])
+end
+
+open(source_json) do |io|
+  source_data = JSON.load(io)
+end
+
+source_data.each do |key,val|
+  target_data.store(
+    key,
+    {
+      "coordinates" => val["coordinates"],
+      "name"        => val["name"],
+      "yomi"        => dump_data[val["name"]]
+    }
+  )
+end
+
+open(target_json, "w") do |io|
+  JSON.dump(target_data, io)
+end
+
